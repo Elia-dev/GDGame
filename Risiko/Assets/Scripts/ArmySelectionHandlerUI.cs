450eecfebc7ae6871c66a2892b7ac3c4cd444e37
Elia-dev <Elia-dev@users.noreply.github.com>
2024-07-25 17:34:23 +0200

    Non bestemmio perchè ho già bestemmiato all'altro push

--- Risiko/Assets/Scripts/ArmySelectionHandlerUI.cs
+++ Risiko/Assets/Scripts/ArmySelectionHandlerUI.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(PolygonCollider2D))] 
-public class ArmySelectionHandlerUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ArmySelectionHandlerUI : MonoBehaviour, IPointerClickHandler//, IPointerEnterHandler, IPointerExitHandler
 {
     private SpriteRenderer sprite;
-    public Color32 oldColor;
+    /*public Color32 oldColor;
     public Color32 hoverColor;
-    public Color32 startColor;
+    public Color32 startColor;*/
+    [SerializeField] private GameObject frame;
     private bool selected = false;
     
     void Awake() {
         sprite = GetComponent<SpriteRenderer>();
-        sprite.color = startColor;
+        //sprite.color = startColor;
+    }
+    /*
+    void OnMouseEnter()  {
+        Debug.Log("Entra?!");
+        if (!selected) {
+            oldColor = sprite.color;
+            sprite.color = hoverColor;
+        }
+    }
+
+    void OnMouseExit() {
+        Debug.Log("Non può entrare!");
+        if (!selected) {
+            sprite.color = oldColor;
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        Debug.Log("Entra?!");
         if (!selected) {
             oldColor = sprite.color;
             sprite.color = hoverColor;
@@ -27,23 +46,20 @@ public class ArmySelectionHandlerUI : MonoBehaviour, IPointerEnterHandler, IPoin
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (!selected) {
-            sprite.color = oldColor;
-        }
-    }
-    /*void OnMouseEnter()  {
-        if (!selected) {
-            oldColor = sprite.color;
-            sprite.color = hoverColor;
-        }
-    }
-
-    void OnMouseExit() {
+        Debug.Log("Non può entrare!");
         if (!selected) {
             sprite.color = oldColor;
         }
     }*/
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        selected = true;
+        Debug.Log("Select");
+        /// CARICARE DATI STATO
+        //sprite.color = hoverColor;
+        frame.SetActive(true);
+    }
     void OnMouseDown()
     {
         if (!selected)
@@ -54,12 +70,16 @@ public class ArmySelectionHandlerUI : MonoBehaviour, IPointerEnterHandler, IPoin
     
     public void Select() {
         selected = true;
+        Debug.Log("Select");
         /// CARICARE DATI STATO
-        sprite.color = hoverColor;
+        //sprite.color = hoverColor;
+        frame.SetActive(true);
     }
 
     public void Deselect() {
+        Debug.Log("Deselect");
         selected = false;
-        sprite.color = startColor;
+        frame.SetActive(false);
+        //sprite.color = startColor;
     }
 }
