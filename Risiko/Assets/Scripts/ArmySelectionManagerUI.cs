fa9695ed4007709919649f77f5a4dc0343721881
Elia-dev <Elia-dev@users.noreply.github.com>
2024-07-26 16:42:13 +0200

    Non funziona il tasto choose

--- Risiko/Assets/Scripts/ArmySelectionManagerUI.cs
+++ Risiko/Assets/Scripts/ArmySelectionManagerUI.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ArmySelectionManagerUI : MonoBehaviour
-{
+public class ArmySelectionManagerUI : MonoBehaviour, IPointerClickHandler {
     public static ArmySelectionManagerUI Instance { get; private set; }
     private ArmySelectionHandlerUI selectedArmy;
 
@@ -13,16 +14,45 @@ public class ArmySelectionManagerUI : MonoBehaviour
     private PointerEventData pointerEventData;
     private EventSystem eventSystem;
 
+    public int playerNumber = 4;
+    [SerializeField] private GameObject redArmy;
+    [SerializeField] private GameObject greenArmy;
+    [SerializeField] private GameObject blueArmy;
+    [SerializeField] private GameObject yellowArmy;
+    [SerializeField] private GameObject purpleArmy;
+    [SerializeField] private GameObject blackArmy;
+    [SerializeField] private TMP_Text title;
+
     private void Awake() {
         if (Instance is null) {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-        } else {
+        }
+        else {
             Destroy(gameObject);
         }
     }
 
     private void Start() {
+        //VA IMPOSTATO IL NUMERO DI GIOCATORI
+        switch (playerNumber) {
+            case 4:
+                yellowArmy.GameObject().SetActive(true);
+                break;
+            case 5:
+                yellowArmy.GameObject().SetActive(true);
+                purpleArmy.GameObject().SetActive(true);
+                break;
+            case 6:
+                yellowArmy.GameObject().SetActive(true);
+                purpleArmy.GameObject().SetActive(true);
+                blackArmy.GameObject().SetActive(true);
+                break;
+        }
+        
+        //SCORRERE LA LISTA DELLE ARMATE GIà PRESE DAGLI ALTRI GIOCATORI E DISATTIVARE I raycastTarget ALLE CORRISPONDENTI ARMATE
+        //greenArmy.GetComponent<Image>().raycastTarget = false;
+        
         // Trova il GraphicRaycaster sul Canvas
         raycaster = GetComponent<GraphicRaycaster>();
 
@@ -30,16 +60,34 @@ public class ArmySelectionManagerUI : MonoBehaviour
         eventSystem = EventSystem.current;
     }
 
-    private void Update() {
-        if (Input.GetMouseButtonDown(0)) {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("UI Element Clicked");
 
-            pointerEventData = new PointerEventData(eventSystem) {
+        // Verifica se il clic è sopra un oggetto con un collider
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Hit object: " + hit.transform.name);
+            // Esegui azioni specifiche se necessario
+        }
+        else
+        {
+            Debug.Log("No hit detected.");
+        }
+    }
+    
+    private void Update() {
+        /*if (Input.GetMouseButtonDown(0)) {
+            pointerEventData = new PointerEventData(eventSystem)
+            {
                 position = Input.mousePosition
             };
 
             List<RaycastResult> results = new List<RaycastResult>();
             raycaster.Raycast(pointerEventData, results);
-
+            
             if (results.Count > 0) {
                 foreach (RaycastResult result in results) {
                     ArmySelectionHandlerUI armyHandlerUI = result.gameObject.GetComponent<ArmySelectionHandlerUI>();
@@ -48,16 +96,18 @@ public class ArmySelectionManagerUI : MonoBehaviour
                         return;
                     }
                 }
-            } else {
+            }
+            else {
                 DeselectArmy();
             }
-        }
+        }*/
     }
 
     public void SelectArmy(ArmySelectionHandlerUI newArmy) {
         if (selectedArmy is not null) {
             selectedArmy.Deselect();
         }
+
         selectedArmy = newArmy;
         selectedArmy.Select();
     }
@@ -66,6 +116,19 @@ public class ArmySelectionManagerUI : MonoBehaviour
         if (selectedArmy is not null) {
             selectedArmy.Deselect();
             selectedArmy = null;
+        }
+    }
+
+    public void ChooseArmy() {
+        if (selectedArmy is not null) {
+            //COMUNICA AL SERVER L'ARMATA
+            //ATTENDE COMUNICAZIONE DAL SERVER PER PASSARE ALLA PROSSIMA FASE
+            //LANCIA LA PROSSIMA VISTA
+            Debug.Log("CANE");
+            gameObject.GetComponent<PopUpSettingsUI>().Close();
+        }
+        else {
+            title.faceColor = Color.red;
         }
     }
 }
