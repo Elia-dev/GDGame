58ee1023d657d1ad5b0d0a550dd30cd17b99358f
Elia-dev <Elia-dev@users.noreply.github.com>
2024-07-31 10:33:49 +0200

    Territoriescards script

--- Risiko/Assets/Scripts/UI/TerritoriesCardsUI.cs
+++ Risiko/Assets/Scripts/UI/TerritoriesCardsUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,9 @@ using UnityEngine.UI;
 public class TerritoriesCardsUI : MoveCardsUI {
     public GameObject imagePrefab; // Prefab dell'immagine da muovere
     public Transform gridTransform; // Transform del Grid Layout Group
+
     public int numberOfImages = 4; // Numero di immagini da creare
+
     //public float speed = 2f; // Velocità di movimento delle immagini
     private List<Territory> territories = new List<Territory>();
 
@@ -19,10 +22,15 @@ public class TerritoriesCardsUI : MoveCardsUI {
         };
         Debug.Log(Screen.width);
         Debug.Log(Screen.height);
-        if((float)Screen.width/(float)Screen.height != )
-        transform.GetComponent<GridLayoutGroup>().cellSize = 
-            new Vector2((Screen.width - 140) / (territori.Length / 2), 
-                (float)((Screen.width - 140) / (territori.Length / 2)*1.33));
+        if ((float)Screen.width / (float)Screen.height < 1.6)
+            transform.GetComponent<GridLayoutGroup>().cellSize =
+                new Vector2((Screen.width - 100) / (territori.Length/3),
+                    (float)((Screen.width - 100) / (territori.Length / 3) * 1.33));
+        else
+            transform.GetComponent<GridLayoutGroup>().cellSize =
+                new Vector2((Screen.width - 140) / (territori.Length / 2),
+                    (float)((Screen.width - 140) / (territori.Length / 2) * 1.33));
+        
         GameObject[] images = new GameObject[territori.Length];
         for (int i = 0; i < territori.Length; i++) {
             images[i] = Instantiate(imagePrefab, transform);
@@ -30,11 +38,11 @@ public class TerritoriesCardsUI : MoveCardsUI {
             images[i].GetComponent<Image>().sprite = imgSprite;
             images[i].transform.SetParent(gridTransform);
             //Debug.Log(images[i].GetComponent<Transform>().position.x + ", " +
-                      //images[i].GetComponent<Transform>().position.y);
+            //images[i].GetComponent<Transform>().position.y);
         }
         //for (int i = 0; i < territori.Length; i++) 
-            //Debug.Log(images[i].GetComponent<Transform>().position.x + ", " +
-                      //images[i].GetComponent<Transform>().position.y);
+        //Debug.Log(images[i].GetComponent<Transform>().position.x + ", " +
+        //images[i].GetComponent<Transform>().position.y);
 
         //StartCoroutine(MoveCards());
     }
@@ -45,7 +53,7 @@ public class TerritoriesCardsUI : MoveCardsUI {
             Debug.Log(targetPosition.x + ", " + targetPosition.y);
             RectTransform imagePrefabRect = imagePrefab.GetComponent<RectTransform>();
             targetPosition.y = -targetPosition.y - imagePrefabRect.rect.height / 2;
-            targetPosition.x = - targetPosition.x + imagePrefabRect.rect.width / 2;
+            targetPosition.x = -targetPosition.x + imagePrefabRect.rect.width / 2;
             //Debug.Log((Screen.height / 2 + imagePrefabRect.rect.height / 2 + 10));
             startPosition = new Vector2(Screen.width / 2, -(Screen.height + imagePrefabRect.rect.height / 2 + 10));
             //for (int i = 0; i < territories.Count; i++) {
@@ -57,10 +65,10 @@ public class TerritoriesCardsUI : MoveCardsUI {
             for (int i = 0; i < territori.Length; i++) {
                 GameObject newImage = Instantiate(imagePrefab, transform);
                 RectTransform rectTransform = newImage.GetComponent<RectTransform>();
-                 //caricamento immagine
-                 loadSprite("Territories/" + territori[i]);
-                 newImage.GetComponent<Image>().sprite = imgSprite;
-                 
+                //caricamento immagine
+                loadSprite("Territories/" + territori[i]);
+                newImage.GetComponent<Image>().sprite = imgSprite;
+
                 // Posiziona l'immagine appena sotto lo schermo
                 rectTransform.anchoredPosition = startPosition;
 
@@ -68,16 +76,16 @@ public class TerritoriesCardsUI : MoveCardsUI {
                 yield return StartCoroutine(MoveImage(newImage, rectTransform));
 
                 //Aggiornamento posizione prossimo territorio
-                targetPosition.x += imagePrefabRect.rect.width + 
+                targetPosition.x += imagePrefabRect.rect.width +
                                     gridTransform.GetComponent<GridLayoutGroup>().spacing.x;
             }
         }
 
         IEnumerator MoveImage(GameObject image, RectTransform rectTransform) {
             float elapsedTime = 0f;
-            while (elapsedTime < moveDuration)
-            {
-                rectTransform.anchoredPosition = Vector2.Lerp(startPosition, targetPosition, elapsedTime / moveDuration);
+            while (elapsedTime < moveDuration) {
+                rectTransform.anchoredPosition =
+                    Vector2.Lerp(startPosition, targetPosition, elapsedTime / moveDuration);
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
