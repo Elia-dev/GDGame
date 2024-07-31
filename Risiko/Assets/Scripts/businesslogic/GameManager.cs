97afbf3542e293d950384e91a1565878ad2cd5a7
Filo00 <filippozaccari@gmail.com>
2024-07-31 12:34:54 +0200

    Update GameManager.cs

--- Risiko/Assets/Scripts/businesslogic/GameManager.cs
+++ Risiko/Assets/Scripts/businesslogic/GameManager.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    /*
+     * - Stato  fantoccio da riempire con le richieste mandate al server.
+     * "esempio" seleziono il canada, quindi chiedo al server info sul canada, oil server mi manda le info che io vado a mettere
+     * nello stato fantoccio
+     *
+     * - Dizionario idPlayer - Nome
+     * - idLobby
+     * - 
+     */
     private Player player;
     public static void ServerConnection(string username)
     {
