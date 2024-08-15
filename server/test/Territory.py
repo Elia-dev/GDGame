0865cec5aed6fe2ffcfcc901b9d524c28fc0083e
Filo00 <filippozaccari@gmail.com>
2024-08-15 17:22:17 +0200

    Update Territory.py

--- server/test/Territory.py
+++ server/test/Territory.py
@@ -19,8 +19,8 @@ class Territory(Card):
 
     @classmethod
     def from_dict(cls, data):
-        return cls(data["id"], data["image"], data["function"], data["description"], data["player_id"],
-                   data["name"], data["num_tanks"], data["continent"])
+        return cls(data["id"], data["Image"], data["Function"], data["Description"], data["player_id"],
+                   data["Name"], data["num_tanks"], data["Continent"])
 
     def __repr__(self):
         return (
