97f9be6f68f4e11b7c1d8a7a26ff7a4ebf3b1a58
Elia-dev <eliamatt2000@gmail.com>
2024-08-18 11:28:41 +0200

    Tasto dadi non interagibile

--- Risiko/Assets/Scripts/UI/DiceControllerUI.cs
+++ Risiko/Assets/Scripts/UI/DiceControllerUI.cs
@@ -3,38 +3,35 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Serialization;
 
-public class DiceControllerUI : MonoBehaviour
-{
-    
-    [SerializeField] private Button DiceButton;
+public class DiceControllerUI : MonoBehaviour {
+    [SerializeField] private Button diceButton;
     [SerializeField] private Animator diceAnimator; // Riferimento all'Animator
-    [SerializeField] private TMP_Text DiceResults;
+    [SerializeField] private TMP_Text diceResults;
     private bool pressedButton = false;
-    GameManager gm = GameManager.Instance;
-    private void Awake()
-    {
-        DiceButton.onClick.AddListener(() =>
-        {
+
+    private void Awake() {
+        diceButton.onClick.AddListener(() => {
             diceAnimator.SetBool("Roll", true);
+            TrowDice();
         });
     }
 
-    private void Update()
-    {
-        if (gm.GetExtractedNumber() != 0 && pressedButton)
-        {
+    private void Update() {
+        if (GameManager.Instance.GetExtractedNumber() != 0 && pressedButton) {
             diceAnimator.gameObject.SetActive(false);
             diceAnimator.SetBool("Roll", false);
-            DiceResults.text = "Extracted number: " + gm.GetExtractedNumber() + "\n Game order: "+ gm.getGame_order();
-            DiceResults.gameObject.SetActive(true);
+            diceResults.text = "Extracted number: " + GameManager.Instance.GetExtractedNumber() + "\n Game order: " 
+                               + GameManager.Instance.getGame_order();
+            diceResults.gameObject.SetActive(true);
             StartCoroutine(WaitAndLoadScene(8f)); //Delay di qualcosa o un tasto per passare di scena
             // GameManager.LoadScene("Main") -> scena della mappa
         }
-            
     }
 
     public void TrowDice() {
+        diceButton.interactable = false;
         StartCoroutine(Wait(4f));
     }
 
@@ -42,9 +39,8 @@ public class DiceControllerUI : MonoBehaviour
         yield return new WaitForSeconds(delay);
         pressedButton = true;
     }
-    
-    IEnumerator WaitAndLoadScene(float delay)
-    {
+
+    IEnumerator WaitAndLoadScene(float delay) {
         // Aspetta il numero di secondi specificato
         yield return new WaitForSeconds(delay);
 
