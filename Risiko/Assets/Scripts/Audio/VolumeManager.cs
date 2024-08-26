c7bf64c0b0f2467a8d7c0d3054bd51b48d19e390
Filo00 <filippozaccari@gmail.com>
2024-08-26 17:36:15 +0200

    Aggiunta barra volume nelle opzioni

---
+++ Risiko/Assets/Scripts/Audio/VolumeManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class VolumeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Slider volumeSlider;
+    public void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+    
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+        Debug.Log("Salvato libello volumen: " + PlayerPrefs.GetFloat("musicVolume"));
+    }
+
+    public void useImp()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+}
