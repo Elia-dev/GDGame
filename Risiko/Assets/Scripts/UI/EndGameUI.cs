29f5eb450ec8fd2b208e021dc4bbe604f2842d7c
Elia-dev <eliamatt2000@gmail.com>
2024-08-27 11:57:08 +0200

    Ok

--- Risiko/Assets/Scripts/UI/EndGameUI.cs
+++ Risiko/Assets/Scripts/UI/EndGameUI.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class EndGameUI : MonoBehaviour {
     [SerializeField] private Button exitButton;
+    [SerializeField] private TMP_Text endGameResult;
     private void Awake() {
         exitButton.onClick.AddListener(() => SceneManager.LoadScene("MainMenu"));
     }
 
     void SetPopUp() {
-        
+        gameObject.SetActive(true);
     }
 }
+
