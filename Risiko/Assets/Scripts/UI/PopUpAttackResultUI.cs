ea1220bbcda6bc1a51ab6b43e2bfec428eaf36fe
Elia-dev <eliamatt2000@gmail.com>
2024-08-29 17:56:31 +0200

    Va bè questo è praticamente cheating se funge

--- Risiko/Assets/Scripts/UI/PopUpAttackResultUI.cs
+++ Risiko/Assets/Scripts/UI/PopUpAttackResultUI.cs
@@ -106,7 +106,7 @@ public class PopUpAttackResultUI : MonoBehaviour {
                 if (enemyExtractedNumbers.Length > i)
                     diceResult.text += myExtractedNumbers[i] + " - " + enemyExtractedNumbers[i];
                 else
-                    diceResult.text += myExtractedNumbers[i] + " -\u200B\u200B\u200B\u200B\u200B\u200B\u200B";
+                    diceResult.text += myExtractedNumbers[i] + "<color=#FFFFFF00> - 0</color>";
                 diceResult.text += "\n";
             }
         }
