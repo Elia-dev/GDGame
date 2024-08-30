5f051243059016acdd088ce775b0b860d5c5d171
Elia-dev <eliamatt2000@gmail.com>
2024-08-30 14:43:40 +0200

    Rinominazione popUpBedName

--- Risiko/Assets/Scripts/UI/PopUpDisplayMessageUI.cs
+++ Risiko/Assets/Scripts/UI/PopUpDisplayMessageUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class PopUpBadNameUI : MonoBehaviour {
+public class PopUpDisplayMessageUI : MonoBehaviour {
     
     [SerializeField] private TMP_Text errorText;
 
