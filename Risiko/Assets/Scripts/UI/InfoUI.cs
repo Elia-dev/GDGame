9c87c8853a5a286af3f4d3529f0783c6c347fac8
Elia-dev <Elia-dev@users.noreply.github.com>
2024-09-06 15:40:50 +0200

    Info

--- Risiko/Assets/Scripts/UI/InfoUI.cs
+++ Risiko/Assets/Scripts/UI/InfoUI.cs
@@ -1,18 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using businesslogic;
 using TMPro;
 using UnityEngine;
 
-public class InfoUI : MonoBehaviour
-{
-    [SerializeField] private TMP_Text playerName;
-    [SerializeField] private TMP_Text playerInfo;
+namespace UI {
+    public class InfoUI : MonoBehaviour {
+        [SerializeField] private GameObject containerInfo;
+        [SerializeField] private Transform contentFather;
 
-    private void OnEnable() {
-        /*foreach (var player in GameManager.Instance.pla) {
-            playerName.color = Utils.ColorCode(GameManager.Instance.GetPlayerColor(GameManager.Instance.id), 255);
-        }*/
+        private void OnEnable() {
+            contentFather.gameObject.SetActive(true);
+            
+            foreach (Transform child in contentFather) {
+                Destroy(child.gameObject);
+            }
+
+            foreach (var playerId in GameManager.Instance.GetPlayersId()) {
+                GameObject newPlayer = Instantiate(contentFather.gameObject, containerInfo.transform);
+                newPlayer.transform.Find("playerName").GetComponent<TMP_Text>().color = Utils.ColorCode(GameManager.Instance.GetPlayerColor(playerId), 255);
+                newPlayer.transform.Find("playerName").GetComponent<TMP_Text>().text = GameManager.Instance.getEnemyNameById(playerId);
+
+                newPlayer.transform.Find("playerInfo").GetComponent<TMP_Text>().text = "Armies Number: " + GameManager.Instance.AllTerritories
+                                      .Where(territory => territory.player_id.Equals(playerId))
+                                      .Sum(territory => territory.num_tanks) + "\n"
+                                  + "Territories Number: " + GameManager.Instance.AllTerritories
+                                      .Count(territory => territory.player_id.Equals(playerId));
+            }
+            
+            contentFather.gameObject.SetActive(false);
+        }
     }
 }
