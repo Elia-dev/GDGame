75c88dabf9757a2faa637203f1dfb8b56c7f0eb6
Elia-dev <Elia-dev@users.noreply.github.com>
2024-09-10 13:34:12 +0200

    Fuck go back

--- Risiko/Assets/Scripts/UI/TerritoriesManagerGamePhaseUI.cs
+++ Risiko/Assets/Scripts/UI/TerritoriesManagerGamePhaseUI.cs
@@ -107,30 +107,25 @@ namespace UI {
 
             //Se è il mio turno e non è in corso nessuna fase e ho dei carri armati da piazzare
             //Inizio la fase di rinforzo abilitando lo script TerritoriesManagerDistrPhaseUI
-            if (_reinforcePhase && !IsPhaseGoing)
+            if (_reinforcePhase && !IsPhaseGoing && Player.Instance.TanksAvailable > 0)
             {
-                Debug.Log
-                    ("Inizio la fase di rinforzo");
-                if (Player.Instance.TanksAvailable > 0 || Player.Instance.Territories.Count >= 3) {
-                    Debug.Log("Ho carri da piazzare");
+                if (Player.Instance.Territories.Count >= 3)
+                {
                     GameManagerUI.ReinforcePhase = true;
                     IsPhaseGoing = true;
                     this.GetComponent<TerritoriesManagerDistrPhaseUI>().enabled = true;
                     GetComponent<TerritoriesManagerDistrPhaseUI>().StartTurn();
                 }
-                else {
-                    Debug.Log("Non ho carri da piazzare");
+                else
+                {
                     //Se invece non ho carri da piazzare paso direttamente alla fase di attacco
-                    ClientManager.Instance.UpdateTerritoriesState();
                     _reinforcePhase = false;
                     GameManagerUI.ReinforcePhase = false;
                     _attackPhase = true;
                     GameManagerUI.AttackPhase = true;
                 }
-                
             }
             else if (_attackPhase && !IsPhaseGoing) {
-                
                 Debug.Log("Inizio la fase di attacco");
                 // Se sono in fase di attacco catturo i click sugli stati e mostro le informazioni
                 endTurnButton.interactable = true;
