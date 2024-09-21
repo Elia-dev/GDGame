0e049ebc590d1fea508d02f5bf863ccbff12bbcf
Filo00 <filippozaccari@gmail.com>
2024-09-21 12:00:03 +0200

    Modificato file sh per salvare stampe del bot su file di log

--- server/run_bot.sh
+++ server/run_bot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 game_id=$1
 bot_name=$2
-/usr/bin/python3 main_bot_controller.py "$game_id" "$bot_name" > /dev/null 2>&1 &
+log_file="bot_${game_id}_${bot_name}.log"
+/usr/bin/python3 main_bot_controller.py "$game_id" "$bot_name" > "$log_file" 2>&1 &
 echo $!
